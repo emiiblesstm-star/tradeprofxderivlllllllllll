7dd357d5b7020527ba144be502b59ec72a03d8cd
emiiblesstm-star <emiiblesstm@gmail.com>
2025-08-20 19:54:57 +0000

    update

--- src/pages/dashboard/constants.ts
+++ src/pages/dashboard/constants.ts
@@ -8,12 +8,11 @@ export type TSidebarItem = {
 
 export const SIDEBAR_INTRO = (): TSidebarItem[] => [
     {
-        label: localize('Welcome to ProfitMax Trader Pro!'),
+        label: localize('Welcome to Emiisdtrader! '),
         content: [
             {
-                data: localize('Trade smarter, not harder – let the bot handle it.'),
+                data: localize('Your Gateway to Smarter, Automated Profits! I want us to be rich!'),
             },
-            { data: localize('Check out these guides and FAQs to learn more about building your bot:') },
         ],
         link: false,
     },
@@ -22,22 +21,5 @@ export const SIDEBAR_INTRO = (): TSidebarItem[] => [
         content: [{ data: localize('Deriv Bot - your automated trading partner') }],
         link: true,
     },
-    {
-        label: localize('FAQs'),
-        content: [
-            {
-                data: localize('What is Deriv Bot?'),
-                faq_id: 'faq-0',
-            },
-            {
-                data: localize('Where do I find the blocks I need?'),
-                faq_id: 'faq-1',
-            },
-            {
-                data: localize('How do I remove blocks from the workspace?'),
-                faq_id: 'faq-2',
-            },
-        ],
-        link: true,
-    },
 ];
+
