8dc103e66f8c9e8c3ab42dc6e4892f9231797fee
emiiblesstm-star <emiiblesstm@gmail.com>
2025-08-25 15:07:33 +0300

    Update dtrader.tsx

--- src/pages/dtrader/dtrader.tsx
+++ src/pages/dtrader/dtrader.tsx
@@ -5,7 +5,7 @@ import IframeWrapper from '@/components/iframe-wrapper';
 const Dtrader = observer(() => {
     return (
         <IframeWrapper
-            src='https://tradeprofx.pages.dev/dtrader?chart_type=area&interval=1t&symbol=1HZ100V&trade_type=accumulator'
+            src='https://derivmaster.pages.dev/?chart_type=area&interval=1t&symbol=1HZ100V&trade_type=accumulator'
             title='DTrader'
             className='dtrader-container'
         />
