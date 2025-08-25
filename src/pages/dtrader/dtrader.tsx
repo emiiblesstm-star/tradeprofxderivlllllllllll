f3eafd3c282aefc45194429fab7aad8a9d46397a
Tonnierich <richtonnie14@gmail.com>
2025-08-25 17:16:31 +0300

    Update dtrader.tsx

--- src/pages/dtrader/dtrader.tsx
+++ src/pages/dtrader/dtrader.tsx
@@ -5,7 +5,7 @@ import IframeWrapper from '@/components/iframe-wrapper';
 const Dtrader = observer(() => {
     return (
         <IframeWrapper
-            src='https://derivmaster.pages.dev/?chart_type=area&interval=1t&symbol=1HZ100V&trade_type=accumulator'
+            src='https://trading-8wrj.vercel.app/'
             title='DTrader'
             className='dtrader-container'
         />
